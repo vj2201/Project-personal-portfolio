fe330851be1dbf96ea6e9ca839d631327a1ce29f
vaibhav.jain <vaibhav.jain@dpc.wa.gov.au>
2024-08-20 15:04:10 +0800

    test

--- infra-stack/main.tf
+++ infra-stack/main.tf
@@ -140,7 +140,3 @@ output "s3_bucket_name" {
 output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.cdn.id
 }
-
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
-}
