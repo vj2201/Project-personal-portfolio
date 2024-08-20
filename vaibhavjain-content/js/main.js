781d22d3aebdfc8f761ab677de954aac26c49011
vaibhav.jain <vaibhav.jain@dpc.wa.gov.au>
2024-08-20 14:33:16 +0800

    new color to website

--- vaibhavjain-content/js/main.js
+++ vaibhavjain-content/js/main.js
@@ -306,7 +306,7 @@
 
         //Google Maps
         if ($(".lmpixels-map")[0]){
-            var address = 'Perth, Western Australia', //Replace with Your Address
+            var address = 'San Francisco, S601 Townsend Street, California, USA', //Replace with Your Address
                 address = encodeURIComponent(address),
                 src = 'https://maps.google.com/maps?q=' + address + '&amp;t=m&amp;z=16&amp;output=embed&amp;iwloc=near&output=embed';
             $(".lmpixels-map iframe").attr("src", src);
